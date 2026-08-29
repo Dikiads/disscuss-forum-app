207d5d1e9363ee7e3cd3aa046d6b079ebedf74f0
Dikiads <dadsnaho@gmail.com>
2026-08-30 06:43:59 +0700

    fix: remove unused import in test to pass eslint

--- src/states/threads/asyncToggleNeutralVoteThread.test.js
+++ src/states/threads/asyncToggleNeutralVoteThread.test.js
@@ -3,7 +3,6 @@ import api from '../../utils/api';
 import {
   asyncToggleNeutralVoteThread,
   toggleNeutralVoteThread,
-  asyncReceiveThreads,
 } from './slice';
 
 describe('asyncToggleNeutralVoteThread thunk', () => {
